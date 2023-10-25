856a64aa9b6156452ba996d4568b6d773cc8a761
vinia <viniusadams98@gmail.com>
2023-10-25 23:51:33 +0100

    adding a few constants

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt6.QtCore import QT_VERSION_STR
+from PyQt6.QtCore import PYQT_VERSION_STR
+
+
+from PyQt6.QtCore import QDate, QTime, QDateTime, Qt
+
+now = QDate.currentDate()
+
+print(now.toString(Qt.DateFormat.ISODate))
+print(now.toString(Qt.DateFormat.RFC2822Date))
+
+datetime = QDateTime.currentDateTime()
+
+print(datetime.toString())
+
+time = QTime.currentTime()
+print(time.toString(Qt.DateFormat.ISODate))
