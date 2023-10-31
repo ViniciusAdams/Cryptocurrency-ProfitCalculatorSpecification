8a7a7fe450274e23d1a962fa6022112861735cf1
vinia <viniusadams98@gmail.com>
2023-10-31 23:14:13 +0000

    adding simple convertor

--- main.py
+++ main.py
@@ -1,17 +1,129 @@
-from PyQt6.QtCore import QT_VERSION_STR
-from PyQt6.QtCore import PYQT_VERSION_STR
+'''
+This file is an edited version of the following tutorial:
+
+"A Currency Converter GUI Program - Python PyQt5 Desktop Application Development Tutorial"
+Git Hub: https://github.com/DarBeck/PyQT5_Tutorial/blob/master/currency_converter.py
+Youtube https://www.youtube.com/watch?v=weKpTw1SjM4 - detailed explanation availble
+
+The edits are marked with the comment 'lecturer_edit' these edits were necessary becasue:
+1. the datasource format has changed since original tutorial publication.
+
+Edited 2019.10.18
+
+'''
+
+import sys
+from urllib.request import urlopen
+from decimal import Decimal
+from PyQt5.QtWidgets import *
 
 
-from PyQt6.QtCore import QDate, QTime, QDateTime, Qt
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-now = QDate.currentDate()
+        date = self.get_data()
+        self.currencies = sorted(self.cur_code)
+        dateLabel = QLabel(date)
+        self.fromComboBox = QComboBox()
+        self.fromComboBox.addItems(self.currencies)
+        self.fromSpinBox = QDoubleSpinBox()
 
-print(now.toString(Qt.DateFormat.ISODate))
-print(now.toString(Qt.DateFormat.RFC2822Date))
+        self.fromSpinBox.setRange(0.01, 10000000.00)
+        self.fromSpinBox.setValue(1.00)
+        self.toComboBox = QComboBox()
+        self.toComboBox.addItems(self.currencies)
+        self.toLabel = QLabel("1.00")
 
-datetime = QDateTime.currentDateTime()
+        grid = QGridLayout()
+        grid.addWidget(dateLabel, 0, 0)
+        grid.addWidget(self.fromComboBox, 1, 0)
+        grid.addWidget(self.fromSpinBox, 1, 1)
+        grid.addWidget(self.toComboBox, 2, 0)
+        grid.addWidget(self.toLabel, 2, 1)
+        self.setLayout(grid)
 
-print(datetime.toString())
+        self.fromComboBox.currentIndexChanged.connect(self.updateUi)
+        self.toComboBox.currentIndexChanged.connect(self.updateUi)
+        self.fromSpinBox.valueChanged.connect(self.updateUi)
 
-time = QTime.currentTime()
-print(time.toString(Qt.DateFormat.ISODate))
+        self.setWindowTitle("Currency Converter")
+
+
+    def updateUi(self):
+        try:
+            to =self.toComboBox.currentText()
+            from_ = self.fromComboBox.currentText()
+            to_code = self.cur_code[to]
+            from_code = self.cur_code[from_]
+            to_amt = Decimal(self.rates[to_code])
+            from_amt = Decimal(self.rates[from_code])
+            amt = Decimal(self.fromSpinBox.value())
+            amount = (from_amt / to_amt) * amt
+            self.toLabel.setText("%.02f" % amount)
+        except Exception as e:
+            print(e)
+
+    def get_data(self):
+        self.cur_code = {"Canadian Dollar": "FXCADCAD"}
+        try:
+            date = "Unknown"
+            file = urlopen("http://www.bankofcanada.ca/valet/observations/group/FX_RATES_DAILY/csv")
+            file_handler = []
+            for row in file:
+                file_handler.append(row.decode().replace('"', '')) # lecturer_edit - replace all the double quotes with empty space
+
+            print(file_handler)
+            print(len(file_handler))
+
+            for row in file_handler:
+                if row.startswith("FX"):
+                    line = row.split(",")
+                    cur = line[2].split(" to")[0]
+                    # cur = cur[1:]                                # lecturer_edit - not needed as we want the whole row including the first element
+                    self.cur_code[cur.title()] = line[0]
+                else:
+                    continue
+            print(self.cur_code)
+
+            header_list = []
+            notFound = True
+            x = 0
+            while notFound:
+                if file_handler[x].startswith("date"):
+                    header = file_handler[x].split(",")
+                    for col in header:
+                        header_list.append(col.strip())
+                    notFound = False
+                x += 1
+            print(header_list)
+
+            data = []
+            for row in file_handler[x:]:
+                if row.startswith("\n"):
+                    break
+                else:
+                    data = row.split(",")
+            print(data)
+
+            i = 0
+            self.rates = {"FXCADCAD": "1.0000"}
+            for d in data:
+                header = "".join(str(header_list[i]))
+                self.rates[header] = d.strip()
+                i += 1
+            print(self.rates)
+            self.rates = self.rates[1:-1]
+            date = self.rates["date"]
+            return "Exchange Rate Date: %s" % date
+
+        except Exception as e:
+            return "Failed to download"
+            print(e)
+        finally:
+            file.close()
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+sys.exit(app.exec_())
